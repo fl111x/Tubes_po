9d4d540df4cf993bbdc755f727cd9f39605c6c13
fl111x <ammar.bfw@gmail.com>
2024-11-15 18:22:42 +0700

    db tubes

---
+++ ATMv1.java
@@ -0,0 +1,10 @@
+class acc{
+    private String accNum;
+    private String pin;
+    private int Saldo;
+
+    public acc(String rekNum, String pin, int Saldo){
+        this.accNum = rekNum;
+    }
+
+}
